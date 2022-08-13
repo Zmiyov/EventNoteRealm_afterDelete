03274db22d43cd8cebd8c23a7982b0ccd90b8859
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-08-13 20:55:16 +0300

    Initial Commit

---
+++ EventNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventNote
+//
+//  Created by Vladimir Pisarenko on 13.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
