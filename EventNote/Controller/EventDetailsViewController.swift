d2c412609cd4d5e3b098268f427e09945255609a
Volodymyr <zmiyov88@gmail.com>
2022-09-03 13:55:02 +0300

    Working with stack in detail

--- EventNote/Controller/EventDetailsViewController.swift
+++ EventNote/Controller/EventDetailsViewController.swift
@@ -12,28 +12,36 @@ class EventDetailsViewController: UIViewController {
     var event: EventRealmModel?
     let dateFormatter = DateFormatter()
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .tertiarySystemBackground
-        title = "Details"
+//        navigationController?.navigationBar.prefersLargeTitles = true
+        dateFormatter.dateFormat = "YYYY, MMMM d"
+        title = dateFormatter.string(from: event!.dateAndTime)
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Dismiss", style: .plain, target: self, action: #selector(dismissSelf))
-        verticalStackView()
+        mainVerticalStackView()
     }
     
     @objc func dismissSelf() {
         dismiss(animated: true, completion: nil)
     }
     
-    func verticalStackView() {
+    func mainVerticalStackView() {
         
         guard let event = event else { return }
 
-        let kindOfShootingLabel = UILabel(text: event.kindOfShooting, font: .systemFont(ofSize: 21, weight: .bold), alighment: .center)
-        
         dateFormatter.dateFormat = "HH:mm"
         let timeOfShooting = dateFormatter.string(from: event.dateAndTime)
         
-        let dateAndTimeLabel = UILabel(text: timeOfShooting, font: .systemFont(ofSize: 21, weight: .bold), alighment: .center)
+        let dateAndTimeLabel = UILabel(text: timeOfShooting, font: .systemFont(ofSize: 30, weight: .bold), alighment: .center)
+        let mainLocationLabel = UILabel(text: "Main location:" + " " + event.mainLocation, font: .systemFont(ofSize: 21, weight: .bold), alighment: .center)
+        
+        let weatherImageView = UIImageView()
+        weatherImageView.image = UIImage(named: "storm.png")
+        
+        let kindOfShootingLabel = UILabel(text: event.kindOfShooting, font: .systemFont(ofSize: 35, weight: .bold), alighment: .left)
         
         let amountOfHoursLabel = UILabel(text: "Amount of hours:" + " " + String(event.amountOfHours), font: .systemFont(ofSize: 21, weight: .bold), alighment: .center)
         
@@ -43,7 +51,7 @@ class EventDetailsViewController: UIViewController {
         let clientTelegramOrChatLabel = UILabel(text: "Telegram:" + " " + event.clientTelegramOrChat, font: .systemFont(ofSize: 21, weight: .bold), alighment: .center)
         let clientInstagramLabel = UILabel(text: "Instagram:" + " " + event.clientInstagram, font: .systemFont(ofSize: 21, weight: .bold), alighment: .center)
         
-        let mainLocationLabel = UILabel(text: "Main location:" + " " + event.mainLocation, font: .systemFont(ofSize: 21, weight: .bold), alighment: .center)
+        
         let startLocationLabel = UILabel(text: "Start location:" + " " + event.startLocation, font: .systemFont(ofSize: 21, weight: .bold), alighment: .center)
         let endLocationLabel = UILabel(text: "End location:" + " " + event.endLocation, font: .systemFont(ofSize: 21, weight: .bold), alighment: .center)
         
@@ -54,77 +62,127 @@ class EventDetailsViewController: UIViewController {
         let alertStringLabel = UILabel(text: "Alert: " + event.alertString, font: .systemFont(ofSize: 21, weight: .bold), alighment: .center)
         
         
-        let verticalStackView = UIStackView()
-        verticalStackView.backgroundColor = .darkGray
-        verticalStackView.axis = .vertical
-        verticalStackView.distribution = .fillEqually
-        verticalStackView.spacing = 1
-        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
         
-        view.addSubview(verticalStackView)
+        
+        let mainVerticalStackView = UIStackView()
+        mainVerticalStackView.backgroundColor = .darkGray
+        mainVerticalStackView.axis = .vertical
+        mainVerticalStackView.distribution = .fill
+        mainVerticalStackView.spacing = 1
+        mainVerticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(mainVerticalStackView)
         NSLayoutConstraint.activate([
-            verticalStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
-            verticalStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
-            verticalStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
-            verticalStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -25),
+            mainVerticalStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            mainVerticalStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            mainVerticalStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            mainVerticalStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -25)
         ])
         
+        
+        let timeLocationWeatherHorStackView = UIStackView()
+        timeLocationWeatherHorStackView.axis = .horizontal
+        timeLocationWeatherHorStackView.distribution = .fillEqually
+        timeLocationWeatherHorStackView.spacing = 1
+        timeLocationWeatherHorStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        
+        let timeLocationVertStackView = UIStackView()
+        timeLocationVertStackView.axis = .vertical
+        timeLocationVertStackView.distribution = .fillEqually
+        timeLocationVertStackView.spacing = 1
+        timeLocationVertStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        
+
+        
+
+        
+        
+        
         if event.kindOfShooting != "" {
-            verticalStackView.addArrangedSubview(kindOfShootingLabel)
+            mainVerticalStackView.addArrangedSubview(kindOfShootingLabel)
+            NSLayoutConstraint.activate([
+                kindOfShootingLabel.topAnchor.constraint(equalTo: mainVerticalStackView.topAnchor, constant: 0),
+                kindOfShootingLabel.leadingAnchor.constraint(equalTo: mainVerticalStackView.leadingAnchor, constant: 0),
+                kindOfShootingLabel.trailingAnchor.constraint(equalTo: mainVerticalStackView.trailingAnchor, constant: 0),
+                kindOfShootingLabel.bottomAnchor.constraint(equalTo: timeLocationWeatherHorStackView.topAnchor, constant: 0),
+                kindOfShootingLabel.heightAnchor.constraint(equalToConstant: 44)
+            ])
         }
         
-        verticalStackView.addArrangedSubview(dateAndTimeLabel)
+        mainVerticalStackView.addSubview(timeLocationWeatherHorStackView)
+        NSLayoutConstraint.activate([
+//            timeLocationWeatherHorStackView.topAnchor.constraint(equalTo: mainVerticalStackView.topAnchor, constant: 0),
+//            timeLocationWeatherHorStackView.leadingAnchor.constraint(equalTo: mainVerticalStackView.leadingAnchor, constant: 0),
+//            timeLocationWeatherHorStackView.trailingAnchor.constraint(equalTo: mainVerticalStackView.trailingAnchor, constant: 0),
+            timeLocationWeatherHorStackView.heightAnchor.constraint(equalToConstant: 150)
+//                kindOfShootingLabel.bottomAnchor.constraint(equalTo: timeLocationWeatherHorStackView.bottomAnchor, constant: 0)
+        ])
+        
+        
+        timeLocationWeatherHorStackView.addSubview(timeLocationVertStackView)
+        NSLayoutConstraint.activate([
+//            timeLocationVertStackView.topAnchor.constraint(equalTo: timeLocationWeatherHorStackView.topAnchor, constant: 0),
+//            timeLocationVertStackView.leadingAnchor.constraint(equalTo: timeLocationWeatherHorStackView.leadingAnchor, constant: 0),
+//            timeLocationVertStackView.trailingAnchor.constraint(equalTo: weatherImageView.trailingAnchor, constant: 0),
+//            timeLocationVertStackView.heightAnchor.constraint(equalToConstant: 300),
+//            timeLocationVertStackView.widthAnchor.constraint(equalToConstant: timeLocationWeatherHorStackView.bounds.width / 2)
+//                kindOfShootingLabel.bottomAnchor.constraint(equalTo: timeLocationWeatherHorStackView.bottomAnchor, constant: 0)
+        ])
+        
+        timeLocationWeatherHorStackView.addSubview(weatherImageView)
+        
+        timeLocationVertStackView.addSubview(dateAndTimeLabel)
+        timeLocationVertStackView.addSubview(mainLocationLabel)
+        
         
         if event.amountOfHours != 0 {
-            verticalStackView.addArrangedSubview(amountOfHoursLabel)
+            mainVerticalStackView.addArrangedSubview(amountOfHoursLabel)
         }
         
         if event.clientName != "" {
-            verticalStackView.addArrangedSubview(nameLabel)
+            mainVerticalStackView.addArrangedSubview(nameLabel)
         }
         
         if event.clientPhoneNumber != "" {
-            verticalStackView.addArrangedSubview(clientPhoneNumberLabel)
+            mainVerticalStackView.addArrangedSubview(clientPhoneNumberLabel)
         }
         
         if event.additionalPhoneNumber != "" {
-            verticalStackView.addArrangedSubview(additionalPhoneNumberLabel)
+            mainVerticalStackView.addArrangedSubview(additionalPhoneNumberLabel)
         }
         
         if event.clientTelegramOrChat != "" {
-            verticalStackView.addArrangedSubview(clientTelegramOrChatLabel)
-        }
-        
-        if event.kindOfShooting != "" {
-            verticalStackView.addArrangedSubview(clientInstagramLabel)
+            mainVerticalStackView.addArrangedSubview(clientTelegramOrChatLabel)
         }
         
         if event.clientInstagram != "" {
-            verticalStackView.addArrangedSubview(mainLocationLabel)
+            mainVerticalStackView.addArrangedSubview(clientInstagramLabel)
         }
         
         if event.startLocation != "" {
-            verticalStackView.addArrangedSubview(startLocationLabel)
+            mainVerticalStackView.addArrangedSubview(startLocationLabel)
         }
         
         if event.endLocation != "" {
-            verticalStackView.addArrangedSubview(endLocationLabel)
+            mainVerticalStackView.addArrangedSubview(endLocationLabel)
         }
         
         if event.fullPrice != "" {
-            verticalStackView.addArrangedSubview(priceLabel)
+            mainVerticalStackView.addArrangedSubview(priceLabel)
         }
         
         if event.priceForHour != "" {
-            verticalStackView.addArrangedSubview(priceForHourLabel)
+            mainVerticalStackView.addArrangedSubview(priceForHourLabel)
         }
         
         if event.prepayment != "" {
-            verticalStackView.addArrangedSubview(prepaymentLabel)
+            mainVerticalStackView.addArrangedSubview(prepaymentLabel)
         }
         
         if event.alertString != "" {
-            verticalStackView.addArrangedSubview(alertStringLabel)
+            mainVerticalStackView.addArrangedSubview(alertStringLabel)
         }
     }
 }
