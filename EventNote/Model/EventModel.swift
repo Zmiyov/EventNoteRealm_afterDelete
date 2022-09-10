83c3b4b73d8fc5997c9af41a9740d9aa8aca9519
Volodymyr <zmiyov88@gmail.com>
2022-09-11 00:05:30 +0300

    Create clear Event Model file

---
+++ EventNote/Model/EventModel.swift
@@ -0,0 +1,38 @@
+//
+//  EventModel.swift
+//  EventNote
+//
+//  Created by Vladimir Pisarenko on 11.09.2022.
+//
+
+import Foundation
+
+class EventModel {
+    
+    var kindOfShooting: String = ""
+    var dateAndTime: Date = Date()
+    var deadlineDate: Date?
+    var deadlineString: String = ""
+    var amountOfHours: Int = 0
+    
+    var clientName: String = ""
+    var clientPhoneNumber: String = ""
+    var additionalPhoneNumber: String = ""
+    var clientTelegramOrChat: String = ""
+    var clientInstagram: String = ""
+    
+    var mainLocation: String = ""
+    var startLocation: String = ""
+    var endLocation: String = ""
+    
+    var priceForHour: String = ""
+    var fullPrice: String = ""
+    var prepayment: String = ""
+    
+    var alertString: String = ""
+    var alertDate: Date?
+    
+    var isDone: Bool = false
+    var identifierID: String = UUID().uuidString
+
+}
