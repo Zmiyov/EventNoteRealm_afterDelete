92dff4a1ee6aa9f4a0707af4b18979312b930726
Volodymyr <zmiyov88@gmail.com>
2022-09-11 12:54:56 +0300

    Add function to saving new entity

--- EventNote/CoreDataDBModel/CoreDataDBEventModel.swift
+++ EventNote/CoreDataDBModel/CoreDataDBEventModel.swift
@@ -10,4 +10,50 @@ import CoreData
 
 class EventEntity: NSManagedObject {
     
+    class func findOrCreate(event: EventModel, context: NSManagedObjectContext) throws -> EventEntity {
+
+        let request: NSFetchRequest<EventEntity> = EventEntity.fetchRequest() as! NSFetchRequest<EventEntity>
+        request.predicate = NSPredicate(format: "identifierID == %d", event.identifierID)
+        
+        do {
+            let events = try context.fetch(request)
+            if events.count > 0 {
+                assert(events.count == 1, "There are few events ralated to one ID")
+                return events[0]
+            }
+        } catch {
+            throw error
+        }
+        
+        let eventEntity = EventEntity(context: context)
+        
+        eventEntity.kindOfShooting = event.kindOfShooting
+        eventEntity.dateAndTime = event.dateAndTime
+        eventEntity.deadlineDate = event.deadlineDate
+        eventEntity.deadlineString = event.deadlineString
+        eventEntity.amountOfHours = Int16(event.amountOfHours)
+        
+        eventEntity.clientName = event.clientName
+        eventEntity.clientPhoneNumber = event.clientPhoneNumber
+        eventEntity.additionalPhoneNumber = event.additionalPhoneNumber
+        eventEntity.clientTelegramOrChat = event.clientTelegramOrChat
+        eventEntity.clientInstagram = event.clientInstagram
+        
+        eventEntity.mainLocation = event.mainLocation
+        eventEntity.startLocation = event.startLocation
+        eventEntity.endLocation = event.endLocation
+        
+        eventEntity.priceForHour = event.priceForHour
+        eventEntity.fullPrice = event.fullPrice
+        eventEntity.prepayment = event.prepayment
+        
+        eventEntity.alertString = event.alertString
+        eventEntity.alertDate = event.alertDate
+        
+        eventEntity.isDone = event.isDone
+        eventEntity.identifierID = event.identifierID
+        
+        return eventEntity
+    }
+    
 }
