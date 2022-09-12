125d7969d39f284259b69faee456ba7384fbe64b
Volodymyr <zmiyov88@gmail.com>
2022-09-12 21:25:17 +0300

    Delete Realm files and unused Core Data files

---
+++ EventNote/Model/EventEntityModel.swift
@@ -0,0 +1,13 @@
+//
+//  EventEntityModel.swift
+//  EventNote
+//
+//  Created by Vladimir Pisarenko on 12.09.2022.
+//
+
+import Foundation
+import CoreData
+
+class EventEntity: NSManagedObject {
+    
+}
